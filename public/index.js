1aa358f889763bf4c5248e4f53492a6dd0f21601
Ioannes <ivan.efimov2281@gmail.com>
2023-01-12 15:31:50 +0300

    Save files

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ const basicCard = document.querySelector('.basic')
 const professionalCard = document.querySelector('.professional')
 const masterCard = document.querySelector('.master')
 
+
 function handleBillingPeriod(){
     let billingPeriodData = billingSwitcher.getAttribute('data-period');
 
@@ -22,13 +23,49 @@ function handleBillingPeriod(){
         }
 
     }
+    handleCosting()
 }
 
-// function handleCosting() {
-//     let basicPrice = basicCard.children
-//     console.log(basicPrice)
-// }
-let basicPrice = basicCard.children
-console.log(basicPrice)
+function handleCosting() {
+    let basicPrice = document.getElementById('basic-price')
+    let professionalPrice = document.getElementById('professional-price')
+    let masterPrice = document.getElementById('master-price')
+    
+    let billingPeriodData = billingSwitcher.getAttribute('data-period');
+    let pricingList = [basicPrice, masterPrice, professionalPrice]
+    switch(billingPeriodData){
+        case 'monthly': {
+            basicPrice.innerHTML = `$19.99`
+            professionalPrice.innerHTML = `$24.99`
+            masterPrice.innerHTML = `$39.99`;
+            pricingList.forEach(e => {
+                e.classList.remove('error-message', 'error')
+            })
+            break;
+        }
+        case `yearly`: {
+            basicPrice.innerHTML = `$${(19.99 * 10).toFixed(1)}9`;
+            professionalPrice.innerHTML = `$${(24.99 * 10).toFixed(1)}9`;
+            masterPrice.innerHTML = `$${(39.99 * 10).toFixed(1)}9`;
+            pricingList.forEach(e => {
+                e.classList.remove('error-message', 'error')
+            })
+            break;
+         }
+        case 'false':{
+            
+            basicPrice.innerHTML = `Please toggle billing period`;
+            professionalPrice.innerHTML = `Please toggle billing period`;
+            masterPrice.innerHTML = `Please toggle billing period`;
+            pricingList.forEach(e => {
+                e.classList.add('error-message', 'error')
+            })
+            break;
+
+        }
+    }
+    
+
+}
 
 billingSwitcher.addEventListener('click', handleBillingPeriod)
